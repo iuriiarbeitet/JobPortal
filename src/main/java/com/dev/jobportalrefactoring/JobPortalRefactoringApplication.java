7e3ccca527f25e3830c3f28e15cbeb696fd7aaaf
RIV <iurii.arbeitet@gmail.com>
2024-11-08 13:27:59 +0100

    first commit

---
+++ src/main/java/com/dev/jobportalrefactoring/JobPortalRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.dev.jobportalrefactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalRefactoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalRefactoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/jobportalrefactoring/JobPortalRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.jobportalrefactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalRefactoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
